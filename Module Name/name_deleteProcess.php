e9eee5cdd9a29d834ef28e311fa80daed7c5b43f
Ashton Power <48048360+powertashton@users.noreply.github.com>
2020-10-15 09:02:10 +0800

    Full Starter Module

---
+++ Module Name/name_deleteProcess.php
@@ -0,0 +1,46 @@
+<?php
+/*
+Gibbon, Flexible & Open School System
+Copyright (C) 2010, Ross Parker
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+include '../../gibbon.php';
+
+include './moduleFunctions.php';
+
+
+
+$URL = $gibbon->session->get('absoluteURL') . '/index.php?q=/modules/' . $gibbon->session->get('module') . '/name.php';
+
+if (isActionAccessible($guid, $connection2, '/modules/Module Name/name_add.php') == false) {
+    //Fail 0
+    $URL = $URL.'&return=error0';
+    header("Location: {$URL}");
+} else {
+    //Proceed!
+    $ID = $_POST['ID']; //The ID of the item you will be deleting
+
+    if ($thing == '') { //Check that your required variables are present
+        //Fail 3
+        $URL = $URL.'&return=error3';
+        header("Location: {$URL}");
+    } else {
+        //Your SQL or Gateway deletee query
+
+        $URL .= "&return=success";
+        header("Location: {$URL}");
+    }
+}
