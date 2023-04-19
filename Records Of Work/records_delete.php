4c6b2b3b05d2f1b3a8fdaf78f4906a89f3c8523f
Kelvin Maina <mainakelvin360@gmail.com>
2023-04-19 13:57:16 +0300

    Records Of Work: add delete feature

--- Records Of Work/records_delete.php
+++ Records Of Work/records_delete.php
@@ -2,27 +2,28 @@
 
 use Gibbon\Forms\Prefab\DeleteForm;
 
-if (isActionAccessible($guid, $connection2, '/modules/Records Of Work/records_delete.php') == false) {
+if (!isActionAccessible($guid, $connection2, '/modules/Records Of Work/records_delete.php') == false) {
     // Access denied
     $page->addError(__('You do not have access to this action.'));
 } else {
     //Proceed!
     $gibbonSchoolYearID = $_GET['gibbonSchoolYearID'] ?? '';
-    $gibbonFormGroupID = $_GET['gibbonCourseClassID'] ?? '';
+    $gibbonCourseClassID = $_GET['gibbonCourseClassID'] ?? '';
+    $workrecordID = $_GET['workrecordID']?? '';
 
-    //Check if gibbonFormGroupID specified
-    if ($gibbonCourseClassID == '' and $gibbonCourseClassID == '') {
-        $page->addError(__('You have not specified one or more required parameters.'));
+    //Check if workrecordID specified
+    if ($workrecordID == '' ) {
+        $page->addError(__('no way ! You have not specified one or more required parameters.'));
     } else {
-            $data = array('gibbonCourseClassID' => $gibbonCourseClassID, 'gibbonSchoolYearID' => $gibbonSchoolYearID);
-            $sql = 'SELECT * FROM recordsOfWork WHERE gibbonCourseClassID=:gibbonCourseClassID AND gibbonSchoolYearID=:gibbonSchoolYearID';
+            $data = array('workrecordID' => $workrecordID);
+            $sql = 'DELETE FROM recordsOfWork WHERE workrecordID=:workrecordID';
             $result = $connection2->prepare($sql);
             $result->execute($data);
 
         if ($result->rowCount() != 1) {
             $page->addError(__('The specified record cannot be found.'));
         } else {
-            $form = DeleteForm::createForm($session->get('absoluteURL').'/modules/'.$session->get('module')."/formGroup_manage_deleteProcess.php?gibbonCourseClassID=$gibbonCourseClassID", true);
+            $form = DeleteForm::createForm($session->get('absoluteURL').'/index.php?q=/modules/'.$session->get('module')."/workRecord_view.php?", true);
             echo $form->getOutput();
         }
     }
